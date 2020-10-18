46823b8547d466784bdea0815c8596ba31e7d753
jsgarzi <jsgarzi@fortitudedevelopment.com>
2020-10-18 15:08:17 -0400

    JS & time block updates

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var currentTime = moment().format("ddd, MMMM Mo, h:mA");
+var valueCompare = moment().format("k");
 
+// Update time / date in the header
 function timeUpdate(){
     var timeEl = $("#currentDay");
     timeEl.text(currentTime);
 }
+
+// compare values against current time - determine past, present, or future block
+function whatTimeIsIt ()
 
 timeUpdate();
